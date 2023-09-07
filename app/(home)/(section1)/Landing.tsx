9501894b85d2d4375070894ee2f88e336d0cb08a
Brandon Bryan <bshwag@gmail.com>
2023-09-07 10:11:00 -0400

    Removed css prop from bg-image in section1

--- app/(home)/(section1)/Landing.tsx
+++ app/(home)/(section1)/Landing.tsx
@@ -5,13 +5,7 @@ import Hero from "./Hero";
 export default function Landing() {
   return (
     <section className="relative z-10 flex w-full items-center justify-center overflow-hidden py-32 md:py-44">
-      <Image
-        className="object-cover"
-        src={"/assets/landing.png"}
-        fill={true}
-        priority={true}
-        alt=""
-      />
+      <Image src={"/assets/landing.png"} fill={true} alt="" />
       <span className="absolute bottom-0 left-0 right-0 h-[36rem] bg-gradient-to-b from-transparent to-black"></span>
       <Hero />
     </section>
